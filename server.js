ea0680c33b1a345d1e7e912a8d10b4eda5b73033
soheon-lee <lsheon93@gmail.com>
2021-03-21 18:25:29 +0900

    Add: CRUD with nodejs express and sequelize

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
 const app = express()
+
+var corsOptions = {
+  origin :"*"
+}
+
+app.use(cors(corsOptions))
 
 app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to >wecode backend application!" })
+  res.json({ message: "Welcome to >wecode backend application!!!!!!!!!!" })
 })
 
+const PORT = process.env.PORT || 8080
 require("./app/routes/customer.routes.js")(app)
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.")
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
 })
